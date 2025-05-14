c277724635a24f27f2a5a1196bfc20c93714178a
Leo <ngarimwenda@gmail.com>
2025-05-14 23:42:07 +0300

    add some type information

--- packages/common/src/getRootPath.js
+++ packages/common/src/getRootPath.js
@@ -4,6 +4,13 @@ import path, { resolve } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/**
+ *
+ * Get the path starting from the root of the kiwi project.
+ *
+ * @param  {...string} args - Arguments of `path.resolve(...args)`
+ * @returns string
+ */
 export default function getRootPath(...args) {
   return resolve(__dirname, '../../../', ...args);
 }
---
+++ packages/common/src/getRoothPath.d.ts
@@ -0,0 +1,1 @@
+export declare function getRootPath(...args: string[]): string;
