806b8c02f426397e3172a2e72f00ab0f0d78bb0e
Leo <ngarimwenda@gmail.com>
2025-05-17 12:33:56 +0300

    move out static code

--- packages/ui/src/Root.tsx
+++ packages/ui/src/Root.tsx
@@ -1,15 +1,16 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { type FC, type PropsWithChildren } from 'react';
 
-export const Root: FC<PropsWithChildren> = (props) => {
-  const colors = {
+const theme = extendTheme({
+  colors: {
     brand: {
       900: '#1a365d',
       800: '#153e75',
       700: '#2a69ac',
     },
-  };
+  },
+});
 
-  const theme = extendTheme({ colors });
+export const Root: FC<PropsWithChildren> = (props) => {
   return <ChakraProvider theme={theme}>{props.children}</ChakraProvider>;
 };
