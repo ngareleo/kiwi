520b5bc7fb66a3c5b649466d5c16eb5b11195f93
Leo <ngarimwenda@gmail.com>
2025-05-17 10:59:28 +0300

    build vite assets in one file to simplify dev server

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import reactLogo from '/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
@@ -12,6 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
